1d7ee4d7fa6ec8f0f469e205be6f6c56922abf6d
Tom Revill <tom.revill@gmail.com>
2017-05-31 17:08:39 +1200

    5.3 Altering the DOM

--- classes.js
+++ classes.js
@@ -1,16 +1,20 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
-function start () {
+function start() {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
+  two()
+  three()
+  makeVisible()
+
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
-function one () {
+function one() {
   // First, we have to find the element:
   var one = document.getElementById('one')
 
@@ -20,6 +24,35 @@ function one () {
 
 // CREATE FUNCTION two HERE
 
+function two() {
+  // First, we have to find the element:
+  var two = document.getElementById('two')
+
+  // Next, we apply a new CSS class to it:
+  two.classList.add('green')
+}
+
+
+
 // CREATE FUNCTION three HERE
 
+function three() {
+  // First, we have to find the element:
+  var three = document.getElementById('three')
+
+  // Next, we apply a new CSS class to it:
+  three.classList.add('red')
+}
+
+
 // CREATE FUNCTION makeVisible HERE
+
+var classVisible = document.getElementsByClassName("invisible");
+
+function makeVisible() {
+  for (var i = 0; i < classVisible.length; i++) {
+    classVisible[i].className = "visible";
+    classVisible[i--].className = "visible";
+
+  }
+}
